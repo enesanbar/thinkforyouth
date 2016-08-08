9b9e4397f6a03aa92d607bff06e4516499f7869e
Enes Anbar <enesanbar@gmail.com>
2016-08-09 02:59:02 +0300

    installed django-dotenv package to read environment variables from .env file

--- thinkforyouth/thinkforyouth/wsgi.py
+++ thinkforyouth/thinkforyouth/wsgi.py
@@ -2,15 +2,15 @@
 WSGI config for thinkforyouth project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
 
+import dotenv
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "thinkforyouth.settings")
+dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "thinkforyouth.settings.local")
 
 application = get_wsgi_application()
